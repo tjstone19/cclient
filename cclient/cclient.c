1f970629ef592d7a581348bcf18734bf401eaae5
tjstone19 <tjstone19@gmail.com>
2015-04-20 23:50:59 -0700

    Changed files to cclient.*

---
+++ cclient/cclient.c
@@ -0,0 +1,9 @@
+//
+//  cclient.c
+//  cclient
+//
+//  Created by T.J. Stone on 4/20/15.
+//  Copyright (c) 2015 T.J. Stone. All rights reserved.
+//
+
+#include "cclient.h"
---
+++ cclient/cclient.h
@@ -0,0 +1,14 @@
+//
+//  cclient.h
+//  cclient
+//
+//  Created by T.J. Stone on 4/20/15.
+//  Copyright (c) 2015 T.J. Stone. All rights reserved.
+//
+
+#ifndef __cclient__cclient__
+#define __cclient__cclient__
+
+#include <stdio.h>
+
+#endif /* defined(__cclient__cclient__) */
