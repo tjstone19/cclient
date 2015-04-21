6d88d46d7153029577a2aaabfd6746cca3c8ea82
tjstone19 <tjstone19@gmail.com>
2015-04-20 23:49:11 -0700

    Initial Commit

---
+++ cclient/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cclient
+//
+//  Created by T.J. Stone on 4/20/15.
+//  Copyright (c) 2015 T.J. Stone. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   printf("Hello, World!\n");
+    return 0;
+}
